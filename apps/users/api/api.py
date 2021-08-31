ad9768344e1625cd9fef854fc0b3b0ff673ed490
Tomas Estrada <tomasenriquea@hotmail.com>
2021-08-31 04:44:23 +0200

    V4 - Modificando la clase UserApiVew del archivo api.py para poder utilizar funciones para las rutas en lugar de clases y para usar el Decorador @API_VIEW. Creando metodo 'user_api_view' y modificando ruta en el archivo 'urls.py', Realizando pruebas. OK

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
-from rest_framework.views import APIView  #
+# from rest_framework.views import APIView  #
 from apps.users.models import User  #
 from apps.users.api.serializers import UserSerializers  #
+from rest_framework.decorators import api_view  # Para usar decorador en las funciones
 
 """
 Este archivo servira para poner los metodos relacionados con las vistas urls, sera para poner los metodos que devolveran
@@ -9,12 +10,25 @@ los datos de la api.
 """
 
 
-class UserAPIView(APIView):
-    def get(self, request):
+# class UserAPIView(APIView):
+#     def get(self, request):
+#         users = User.objects.all()  # Se obtiene todos los datos de la BBDD
+#         """
+#         Se serializa lo obtenido. Con 'many' se indica que hay que serializar todos los elementos a JSON, si esto no
+#         estubiera solo serviria para un solo objeto.        """
+#         users_serializer = UserSerializers(users, many=True)
+#         # Aqui se devuelve los datos serializados o convertidos a JSON usando la el '.data'
+#         return Response(users_serializer.data)
+
+
+# Este decorador sirve para indicar los metodos http permiten la función y van en una lista
+@api_view(['GET', 'POST'])
+def user_api_view(request):
+    if request.method == 'GET':
         users = User.objects.all()  # Se obtiene todos los datos de la BBDD
-        """
-        Se serializa lo obtenido. Con 'many' se indica que hay que serializar todos los elementos a JSON, si esto no 
-        estubiera solo serviria para un solo objeto.        """
         users_serializer = UserSerializers(users, many=True)
         # Aqui se devuelve los datos serializados o convertidos a JSON usando la el '.data'
         return Response(users_serializer.data)
+
+    elif request.method == 'POST':
+        print(request.data)  # Para probar que los datos llegan al servidor, ver en la consola
--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from apps.users.api.api import UserAPIView
+# from apps.users.api.api import UserAPIView  # Aqui se esta importando la clase que sirve como ruta para el JSON
+from apps.users.api.api import user_api_view  # Metodo con decorador para el JSON
 
 """
 Este archivo servira para poner las rutas urls que estan enlazadas con el archivo urls.py del proyecto principal
 """
 
 urlpatterns = [
-    path('usuario/', UserAPIView.as_view(), name='usuario_api'),
+    # path('usuario/', UserAPIView.as_view(), name='usuario_api'),
+    path('usuario/', user_api_view, name='usuario_api'),  # Metodo con decorador para la ruta
 ]
